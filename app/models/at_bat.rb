7d27a046ebb02b7816301a29803045c22978f0c9
jrectenwald <jimrec101@gmail.com>
2015-10-24 19:36:18 -0400

    remove duplicate scraper.rb file

--- app/models/at_bat.rb
+++ app/models/at_bat.rb
@@ -5,5 +5,21 @@ class AtBat < ActiveRecord::Base
 ).xpath("//atbat")
   end
 
-
+  def 
 end
+
+
+
+
+
+  # def get_pitches_data_array
+  #   pitches = get_pitches
+  #   pitches_array = []
+  #   pitches.each do |pitch|  
+  #     pitch_hash = pitch.each_with_object({}) do |pitch, hash| 
+  #       hash[pitch[0]] = pitch[1] 
+  #     end 
+  #     pitches_array << pitch_hash
+  #   end 
+  #   return pitches_array
+  # end
